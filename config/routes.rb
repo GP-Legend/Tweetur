0033dc299795ec3db5dfbc19168c1d9a34edf39d
GP-Legend <113549664+GP-Legend@users.noreply.github.com>
2022-10-04 18:59:00 -0700

    Uploaded App Files
    The bulk of the application has been uploaded. The rest is coming... right now!

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users, :controllers => { registrations: 'registrations'}
+
+  resources :tweeets
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+
+  root 'tweeets#index'
+end
